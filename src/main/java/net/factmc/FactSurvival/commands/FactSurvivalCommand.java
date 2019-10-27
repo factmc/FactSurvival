500aacd2cab5d6133636f6d0bede89b5a60c2d27
TrollyLoki <trollyloki@factmc.net>
2019-10-27 12:38:35 -0400

    Add custom tab completion

--- src/main/java/net/factmc/FactSurvival/commands/FactSurvivalCommand.java
+++ src/main/java/net/factmc/FactSurvival/commands/FactSurvivalCommand.java
@@ -1,19 +1,27 @@
 package net.factmc.FactSurvival.commands;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import net.factmc.FactCore.CoreUtils;
 import net.factmc.FactSurvival.RecipeManager;
 import net.factmc.FactSurvival.listeners.CompassController;
 
-public class FactSurvivalCommand implements CommandExecutor {
+public class FactSurvivalCommand implements CommandExecutor, TabExecutor {
 	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("fsur")) {
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("fsur")) {
+        	
         	if (!sender.hasPermission("factsurvival.admin")) return false;
         	
         	if (args.length > 0) {
@@ -41,7 +49,7 @@ public class FactSurvivalCommand implements CommandExecutor {
         				return false;
         			}
         			
-        			ItemStack item = getItem(args[1]);
+        			ItemStack item = getItems().get(args[1]);
         			if (item == null) {
         				sender.sendMessage(ChatColor.RED + args[1] + " is not known");
         				return false;
@@ -59,24 +67,43 @@ public class FactSurvivalCommand implements CommandExecutor {
         	return false;
         	
         }
+        
 		return false;   
     }
 	
-	private static ItemStack getItem(String item) {
-		
-		switch (item) {
-		
-		case "village_tracker":
-			return RecipeManager.villageTracker;
-		case "fortress_tracker":
-			return RecipeManager.fortressTracker;
-		case "city_tracker":
-			return RecipeManager.cityTracker;
-		default:
-			return null;
-		
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		if (command.getName().equalsIgnoreCase("fsur")) {
+			
+			if (args.length < 2) return CoreUtils.filter(CoreUtils.toList("reload", "give"), args[0]);
+			
+			else if (args[0].equalsIgnoreCase("give")) {
+				
+				if (args.length == 2) return CoreUtils.filter(getItemNames(), args[1]);
+				
+			}
+			
+			return CoreUtils.toList();
+			
 		}
 		
+		return null;
+	}
+	
+	private static Map<String, ItemStack> getItems() {
+		Map<String, ItemStack> map = new HashMap<String, ItemStack>();
+		map.put("village_tracker", RecipeManager.villageTracker);
+		map.put("fortress_tracker", RecipeManager.fortressTracker);
+		map.put("city_tracker", RecipeManager.cityTracker);
+		return map;
+	}
+	
+	private static List<String> getItemNames() {
+		List<String> list = new ArrayList<String>();
+		for (String key : getItems().keySet()) {
+			list.add(key);
+		}
+		return list;
 	}
 
 }
